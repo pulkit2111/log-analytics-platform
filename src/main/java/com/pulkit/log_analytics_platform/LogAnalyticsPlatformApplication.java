db93feeee21da717c694dd596396dc1b4a2deef7
Pulkit Mangla <pulkitmangla.2111@gmail.com>
2026-07-22 14:29:13 +0530

    Intialized project and integrated Postgres and redis.

---
+++ src/main/java/com/pulkit/log_analytics_platform/LogAnalyticsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.pulkit.log_analytics_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulkit/log_analytics_platform/LogAnalyticsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulkit.log_analytics_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
